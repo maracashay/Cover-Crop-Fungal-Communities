9393bae376100deeebdae0ad0f125f50f5828071
Mara Cloutier <muc345@psu.edu>
2020-03-30 13:41:52 -0400

    Add files via upload

---
+++ detrended-correspondence-analysis-code.R
@@ -0,0 +1,9 @@
+#### Axis graduent lengths #####
+library(vegan)
+decorana(AMFungi)
+decorana(insects.df)
+rare.t <- t(rare)
+decorana(rare.t)
+decorana(abund_table_allotus)
+decorana(core)
+decorana(all.t)
